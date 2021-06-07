ee21bb6de15935b7c2019cb559c54ba4bb3d1e80
XiosBombay <OMGmsebastian@gmail.com>
2021-06-07 12:45:31 -0500

    Feat: Current Team Page

--- js/mobile.js
+++ js/mobile.js
@@ -1,12 +1,25 @@
-const doc = document;
-const menuOpen = doc.querySelector(".menu");
-const menuClose = doc.querySelector(".close");
-const overlay = doc.querySelector(".overlay");
-
-menuOpen.addEventListener("click", () => {
-  overlay.classList.add("overlay--active");
+document.querySelector(".menu").addEventListener("click", () => {
+  document.querySelector(".overlay").classList.add("overlay--active");
 });
 
-menuClose.addEventListener("click", () => {
-  overlay.classList.remove("overlay--active");
+document.querySelector(".close").addEventListener("click", () => {
+  document.querySelector(".overlay").classList.remove("overlay--active");
 });
+
+let currentSlide = 0;
+
+const changingSlide = () => {
+  let i;
+  const slides = document.getElementsByClassName("team-slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  currentSlide++;
+  if (currentSlide > slides.length) {
+    currentSlide = 1
+  }    
+  slides[currentSlide - 1].style.display = "block";
+  setTimeout(changingSlide, 5000);
+}
+
+changingSlide();
