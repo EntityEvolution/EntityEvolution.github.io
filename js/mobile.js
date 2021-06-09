8f14bf91532297ca87d5fcff94651506a8e1236a
XiosBombay <OMGmsebastian@gmail.com>
2021-06-09 10:10:06 -0500

    Fix: Lighthouse errors

--- js/mobile.js
+++ js/mobile.js
@@ -1,3 +1,4 @@
+
 document.querySelector(".menu").addEventListener("click", () => {
   document.querySelector(".overlay").classList.add("overlay--active");
 });
@@ -6,11 +7,57 @@ document.querySelector(".close").addEventListener("click", () => {
   document.querySelector(".overlay").classList.remove("overlay--active");
 });
 
+// Chancing slides
 let currentSlide = 0;
+let i;
+let stopAnimation = false;
 
+const allId = document.querySelectorAll("#all-images div");
+const slides = document.getElementsByClassName("team-slides");
+
+document.getElementById("after").addEventListener("click", () => {
+  currentSlide++
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  if (currentSlide > slides.length) {
+    currentSlide = 1
+  }
+  slides[currentSlide - 1].style.display = "block";
+  slides[currentSlide - 1].style.animation = "fadeIn 1.5s linear";
+});
+
+document.getElementById("before").addEventListener("click", () => {
+  currentSlide--
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  if (currentSlide < 1) {
+    currentSlide = slides.length
+  }
+  slides[currentSlide - 1].style.display = "block";
+  slides[currentSlide - 1].style.animation = "fadeIn 1.5s linear";
+});
+
+
+for (var e = 0; e < allId.length; e++) {
+  const allImage = allId[e].id
+  document.getElementById(allImage).onmouseover = function () {
+    stopAnimation = true;
+    clearTimeout(clockTime)
+  }
+  document.getElementById(allImage).onmouseout = function () {
+    stopAnimation = false;
+    clockTime = setTimeout(changingSlide, 5000);
+  }
+}
+
+// Constantly moving slides
 const changingSlide = () => {
-  let i;
-  const slides = document.getElementsByClassName("team-slides");
+  if (stopAnimation) {
+    console.log("a")
+    return
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
@@ -20,7 +67,7 @@ const changingSlide = () => {
   }
   slides[currentSlide - 1].style.display = "block";
   slides[currentSlide - 1].style.animation = "fadeIn 1.5s linear";
-  setTimeout(changingSlide, 5000);
+  clockTime = setTimeout(changingSlide, 5000);
 }
 
 changingSlide();
