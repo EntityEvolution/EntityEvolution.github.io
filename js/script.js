76750a5bdfca98f9a29ac79be47cf9caef8168ff
BombayV <OMGmsebastian@gmail.com>
2022-11-06 18:22:30 -0500

    sidebar

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 "use strict";
 import SpaceTravel from './classes/SpaceTravel.js';
 
-console.log(PROD)
-
 let canvas = null;
 let skip = null;
 
 window.addEventListener('load', () => {
+  // Elems
   canvas = document.getElementById('canvas');
   skip = document.getElementById('skip');
   const mobRefresh = document.getElementById('refresh-mob');
+  const mobSidebar = document.getElementById('sidebar-mob');
+  const sidenav = document.getElementById('sidenav');
+  const sidenavClose = document.getElementById('sidenav-close');
+
+  // three.js init
   const spaceTravel = new SpaceTravel(canvas, 200);
   if (spaceTravel.getWebGlSupport()) {
     supportsWebGL(spaceTravel);
@@ -19,9 +23,22 @@ window.addEventListener('load', () => {
     hideCanvas();
   }
 
+  // Listeners
   mobRefresh.addEventListener('click', (e) => {
     e.preventDefault();
     location.reload();
+  });
+
+  mobSidebar.addEventListener('click', (e) => {
+    e.preventDefault();
+    // Change data-active attribute
+    sidenav.setAttribute('data-active', sidenav.getAttribute('data-active') === 'true' ? 'false' : 'true');
+  });
+
+  sidenavClose.addEventListener('click', (e) => {
+    e.preventDefault();
+    // Change data-active attribute
+    sidenav.setAttribute('data-active', 'false');
   });
 });
 
@@ -35,6 +52,12 @@ const removeText = (cb) => {
       cb();
     }
   };
+}
+
+const debugMode = () => {
+  canvas.style.display = 'none';
+  document.getElementById('wrapper').style.display = 'flex';
+  document.querySelector('.slide-down').style.opacity = '1';
 }
 
 const hideCanvas = () => {
